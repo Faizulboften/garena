582dac9ec4336b9bc01cfd373398bc2d6b583f2f
Faizulboften <72049391+Faizulboften@users.noreply.github.com>
2020-10-04 14:11:30 +0800

    Create jos.sh

---
+++ jos.sh
@@ -0,0 +1,47 @@
+clear
+echo $cyan "🔓 INSTALL SERVER"
+sleep 10
+
+bi='\033[34;1m' #biru
+ij='\033[32;1m' #ijo
+pr='\033[35;1m' #purple
+cy='\033[36;1m' #cyan
+me='\033[31;1m' #merah
+pu='\033[37;1m' #putih
+ku='\033[33;1m' #kuning
+or='\033[1;38;5;208m' #Orange
+echo $ku "
+╦ ╦╦ ╦╔═╗╔╦╗╔═╗╔═╗╔═╗╔═╗ 🔓 : 082271426251
+║║║╠═╣╠═╣ ║ ╚═╗╠═╣╠═╝╠═╝ 🔓 : Faizul
+╚╩╝╩ ╩╩ ╩ ╩ ╚═╝╩ ╩╩  ╩"   🔓 : Kocok
+
+sleep 10
+echo $cyan " 🔴 MENGINSTALL"
+sleep 20
+echo $cyan " 🔴 CEK"
+sleep 1
+ccho $cyan " 🔴 CEK "
+sleep 1
+echo $cyan " 🔴 CEK "
+sleep 1
+echo $cyan " 🔴 CEK"
+sleep 1
+ccho $cyan " 🔴 CEK "
+sleep 1
+echo $cyan " 🔴 CEK"
+sleep 1
+echo $cyan " 🔴 CEK"
+sleep 1
+ccho $cyan " 🔴 CEK "
+sleep 1
+echo $cyan " 🔴 CEK "
+sleep 1
+echo $cyan " 🔴 CEK"
+sleep 1
+ccho $cyan " 🔴 CEK "
+sleep 1
+echo $cyan " 🔴 CEK"
+sleep 10
+echo $cyan " 🔴 MASUK "
+
+termux-open https://soo.gd/FC5H
